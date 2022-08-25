41c3e30bd6bbee16972e32b3de702043844c7220
mustafa <hana.hadzo@authoritypartners.com>
2022-08-25 10:50:16 +0200

    Email and Password Generator

--- cypress/e2e/userRegister/register.cy.js
+++ cypress/e2e/userRegister/register.cy.js
@@ -1,11 +1,11 @@
 import {userRegister} from "../../support/commands";
-
+import {emailGenerator, passwordGenerator} from "../../support/helper";
 
 it('User Inputs Fields', () => {
 
     cy.visit("https://dev.parserr.com/account/register")
 
-   userRegister("Mustafa", "Mulalic", "242525", "random@mailinato.com", "testuser")
+   userRegister("Mustafa", "Mulalic", "242525", emailGenerator(), passwordGenerator())
 
 
 })
