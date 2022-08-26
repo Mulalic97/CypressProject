6a0995f491674ea6dd04a6ec4823fd9e6134d5ee
mustafa <hana.hadzo@authoritypartners.com>
2022-08-26 15:21:46 +0200

    Env variables

--- cypress/support/helper.js
+++ cypress/support/helper.js
@@ -1,10 +1,10 @@
 const RandExp = require('randexp');
 
 export function emailGenerator(){
-   return new RandExp(/^[a-z0-9._+-]{1,20}@[a-z0-9]{3,15}\.[a-z]{2,4}$/).gen()
+   return new RandExp(/^[a-z0-9]{1,20}@mailinator\.com$/).gen()
 }
 
 
 export function passwordGenerator() {
-   return new RandExp(/^[-+]?[0-9]{0,16}(\.[0-9]{1,6})?$/).gen()
+   return new RandExp(/^[a-zA-Z0-9_/!@#$%^&*()+={}><.,~`?"':;]{16}$/gi).gen()
 }
